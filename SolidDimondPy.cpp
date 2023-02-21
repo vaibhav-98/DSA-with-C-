bc0fac6ad0530fdc9b5b0159b2db1aa42279d048
vaibahv-98 <vaibhavtriapthi1512@gmail.com>
2023-02-21 17:19:29 +0530

    added code

---
+++ SolidDimondPy.cpp
@@ -0,0 +1,38 @@
+#include<iostream> 
+using namespace std ;
+
+int main () {
+
+    int n ;
+    cin >> n ;
+
+    //outer loop 
+    for ( int row =0 ; row < n ; row +=1) {
+
+        //inner loop
+        for ( int col = 0 ; col < n -row -1 ; col +=1) {
+            // space 
+            cout << " " ;
+        }
+        //star 
+        for ( int col = 0 ; col < row + 1 ; col +=1) {
+
+            cout << "* " ;
+        }
+        cout << endl ;
+    }
+    //outer loop
+    for ( int row = 0 ; row < n ; row +=1) {
+     // inner loop
+     for (int col = 0 ; col < row ; col +=1) {
+        //space 
+        cout << " ";
+     }
+     for ( int col = 0 ; col < n - row ; col += 1) {
+         // star 
+          cout <<"* " ;
+     }
+     cout << endl ;
+
+    }
+}
