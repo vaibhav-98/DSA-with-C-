67885b52df616cef09a0a3ccdece9e8a4f8c9ce5
vaibahv-98 <vaibhavtriapthi1512@gmail.com>
2023-02-21 15:29:08 +0530

    added

---
+++ InvertedHalfPyramid.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std ;
+
+
+int main () {
+
+    int n ;
+    cin >> n ;
+
+    for ( int row = 0 ; row < n ; row +=1) {
+
+        for ( int col = 0 ; col < n -row ; col +=1) {
+
+            cout << " * ";
+        }
+        cout << endl ;
+    }
+}
