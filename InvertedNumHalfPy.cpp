08c228e66884dd68f8527deffa4b38cd95a7f913
vaibahv-98 <vaibhavtriapthi1512@gmail.com>
2023-02-21 15:48:56 +0530

    added

---
+++ InvertedNumHalfPy.cpp
@@ -0,0 +1,17 @@
+#include<iostream> 
+using namespace std ;
+
+int main () {
+
+    int n ;
+    cin >> n ; 
+
+    for (int row = 0 ; row < n ; row +=1) {
+
+        for ( int col = 0 ; col < n - row ; col +=1) {
+
+            cout << col + 1 ;
+        }
+        cout << endl ;
+    }
+}
