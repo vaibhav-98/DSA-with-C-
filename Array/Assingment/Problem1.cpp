f696b9d55bc0d67b7eb11a9fa2dc4d953b2b68bf
vaibahv-98 <vaibhavtriapthi1512@gmail.com>
2023-06-14 12:03:04 +0530

    code added

---
+++ Array/Assingment/Problem1.cpp
@@ -0,0 +1,37 @@
+//====================== printing a 2D array ===============================
+#include <iostream>
+using namespace std;
+
+
+
+void printrowArray(int arr [][3], int row , int col){
+
+
+    for (int i=0; i< row ; i++){
+
+        for (int j=0; j<col; j++){
+            cout << arr[i][j]<<" ";
+        }
+        cout <<endl;
+    }
+}
+
+int main()
+{
+
+    int arr[3][3];
+    int row = 3;
+    int col = 3;
+
+    // row wise input
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+
+   
+   printrowArray(arr,row,col);
+}
