90656a2fdcd195a67800f3f7681c0e31ea54fbd4
vaibahv-98 <vaibhavtripathi1512@gmail.com>
2023-06-17 16:55:25 +0530

    problem 4

---
+++ BinarySearch/Problem4.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<vector>
+using namespace std  ;
+
+int lastOcc(vector<int>arr , int target) {
+
+    int start = 0;
+    int end = arr.size() - 1 ;
+    int mid = start + ( end - start) / 2;
+    int ans = -1 ;
+
+    while (start<=end) {
+        if (arr[mid]==target) {
+            ans = mid;
+            // right search
+            start = mid + 1;
+        }
+        else if (target < arr[mid])
+        {
+            // left search
+            end = mid - 1;
+        }
+        else if (target > arr[mid])
+        {
+            //right search
+            start = mid +1;
+        }
+        mid =  start+(end  -start)/2;
+        
+    }
+    return ans ;
+}
+
+
+int main () {
+
+vector<int> v{1,3,7,7,7,7,7,7,7,7,7,7,7,7,9};
+int target = 7;
+
+ int ans = lastOcc(v,target);
+
+ cout<<" last Occ = "<< ans << endl;
+
+ return 0 ;
+
+}
+
+
+
+
+
+
+
+
+
+
