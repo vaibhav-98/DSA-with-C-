78f27712b19812cd76606e30977631246e7c398d
vaibahv-98 <vaibhavtripathi1512@gmail.com>
2023-06-20 09:13:49 +0530

    code push

--- Array/Assingment/Problem2.cpp
+++ Array/Assingment/Problem2.cpp
@@ -7,7 +7,7 @@ void FindMissing(int*arr , int n) {
 for (int i =0 ; i<n ; i++) {
     int index = abs(arr[i]);
     if(arr[index-1] > 0) {
-        arr[index] *= -1;
+        arr[index -1] *= -1;
     }
 }
 
